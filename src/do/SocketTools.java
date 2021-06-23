1e0ed3ab1fbcc16290aacce2efada5c7c855ea83
Polairis111 <1292199899@qq.com>
2021-06-23 08:36:27 +0800

    This is a toolkit for socket.

---
+++ src/do/SocketTools.java
@@ -0,0 +1,12 @@
+package do;
+
+import java.net.DatagramSocket;
+import java.net.Socket;
+public class SocketTools {
+    public static void main(String[] args) {
+
+    }
+    public void sendMessage(DatagramSocket datagramSocket,String message) {
+        
+    }
+}
